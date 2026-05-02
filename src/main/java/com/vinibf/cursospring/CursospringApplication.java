dd6a0e007e8c2a708337351eea09ed8531a2ee6a
Vinicius Ferrari <viniciusbinderferrari51@gmail.com>
2026-05-02 19:33:03 -0300

    Projeto criado

---
+++ src/main/java/com/vinibf/cursospring/CursospringApplication.java
@@ -0,0 +1,14 @@
+package com.vinibf.cursospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringApplication.class, args);
+		System.out.println("Funcionou!");
+	}
+
+}
---
+++ src/test/java/com/vinibf/cursospring/CursospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinibf.cursospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
