eb99fda0834288feef78f5c98a4226e0e1b45f4b
Vinicius Ferrari <viniciusbinderferrari51@gmail.com>
2026-05-02 20:06:20 -0300

    primeira entity e resource adicionadas para teste

---
+++ src/main/java/com/vinibf/cursospring/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.vinibf.cursospring.resources;
+
+import com.vinibf.cursospring.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User user = new User(1, "Teste", "Teste@gmail.com", "123", "senha123");
+        return ResponseEntity.ok().body(user);
+    }
+
+}
