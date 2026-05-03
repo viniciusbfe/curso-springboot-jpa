054f410f9ce21f7488af5c1e3cb310d68d5624e3
Vinicius Ferrari <viniciusbinderferrari51@gmail.com>
2026-05-02 22:06:58 -0300

    adicionada entidade Order e relacionamento OneToMany/ManyToOne

--- src/main/java/com/vinibf/cursospring/entities/User.java
+++ src/main/java/com/vinibf/cursospring/entities/User.java
@@ -3,6 +3,8 @@ package com.vinibf.cursospring.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -17,6 +19,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -81,5 +86,7 @@ public class User implements Serializable {
         return Objects.hashCode(id);
     }
 
-
+    public List<Order> getOrders() {
+        return orders;
+    }
 }
