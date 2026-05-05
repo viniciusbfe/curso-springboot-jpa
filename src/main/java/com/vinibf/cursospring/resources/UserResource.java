f627bef3db40b9ba69a9c08d86de4c0c7bee89d2
Vinicius Ferrari <viniciusbinderferrari51@gmail.com>
2026-05-04 22:13:07 -0300

    user insert, delete e update

--- src/main/java/com/vinibf/cursospring/resources/UserResource.java
+++ src/main/java/com/vinibf/cursospring/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.vinibf.cursospring.entities.User;
 import com.vinibf.cursospring.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,6 +26,25 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findByID(@PathVariable Integer id){
         User user = service.findByID(id);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insertUser(@RequestBody User user){
+        user = service.insertUser(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Integer id){
+        service.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Integer id, @RequestBody User user){
+        user = service.updateUser(id, user);
         return ResponseEntity.ok().body(user);
     }
 
--- src/main/java/com/vinibf/cursospring/services/UserService.java
+++ src/main/java/com/vinibf/cursospring/services/UserService.java
@@ -21,4 +21,24 @@ public class UserService {
     public User findByID(Integer id){
         return repository.findById(id).orElseThrow(() -> new RuntimeException("User not found."));
     }
+
+    public User insertUser(User user){
+        return repository.save(user);
+    }
+
+    public void deleteUser(Integer id){
+        repository.deleteById(id);
+    }
+
+    public User updateUser(Integer id, User user){
+        User entity = repository.getReferenceById(id);
+        updateData(entity, user);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User user) {
+        entity.setName(user.getName());
+        entity.setEmail(user.getEmail());
+        entity.setPhone(user.getPhone());
+    }
 }
